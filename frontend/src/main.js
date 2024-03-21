bc3806e478b38e0db00907fef8e98383f80877e6
Kira1108 <327485253@163.com>
2024-03-21 17:51:26 +0800

    fix minor bugs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,11 @@ import store from './store'
 // import './assets/js/rem'
 import './assets/css/base.css'
 import './assets/css/mine.css'
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 import axios from 'axios'
-axios.defaults.baseURL = "http://web:8000"; // 开发线上环境
+
+axios.defaults.baseURL = "http://localhost:8004"; // 开发线上环境
 // axios.defaults.baseURL = "http://localhost:8000"; // 开发线下本地环境
 import api from "./views/api/api";
 Vue.prototype.api = api;
@@ -23,3 +24,11 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+
+
+
+
+
+// Rest of your code...
