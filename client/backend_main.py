898596a12044cfb37520c0e64aa89a1cd1e2e7c1
Kira1108 <327485253@163.com>
2024-03-25 17:14:22 +0800

    Initial commit

--- client/backend_main.py
+++ client/backend_main.py
@@ -3,7 +3,12 @@ from PIL import Image
 import io
 import base64
 import os
-from schemas import AbnWindowInfo, OcrResponse, TextInfo, SexInfo
+from schemas import (
+    AbnWindowInfo, 
+    OcrResponse, 
+    TextInfo, 
+    SexInfo
+)
 
 def read_b64(path):
     # Open image
@@ -27,55 +32,70 @@ class BackendClient:
         self.ocr    = "http://localhost:8004/image/ocr"
         self.all    = "http://localhost:8004/image/all"
         self.dfa    = "http://localhost:8004/text/dfa" 
+        self.video  = "http://localhost:8004/video/videofile"
         
-    def predict_ocr(self, path):
+    def predict_ocr(self, path:str, ip_address:str):
         img_b64 = read_b64(path)
-        response = requests.post(self.ocr, json={"image": str(img_b64), 'ip_address': "cctv-005-01"})
+        response = requests.post(self.ocr, json={"image": str(img_b64), 'ip_address': ip_address})
         return OcrResponse(**response.json())
     
-    def predict_window(self, path):
+    def predict_window(self, path:str, ip_address:str):
         img_b64 = read_b64(path)
-        response = requests.post(self.window, json={"image": str(img_b64), 'ip_address': "cctv-005-01"})
+        response = requests.post(self.window, json={"image": str(img_b64), 'ip_address': ip_address})
         return AbnWindowInfo(**response.json())
     
     
-    def predict_sex(self, path):
+    def predict_sex(self, path:str, ip_address:str):
         img_b64 = read_b64(path)
-        response = requests.post(self.sex, json={"image": str(img_b64), 'ip_address': "cctv-005-01"})
+        response = requests.post(self.sex, json={"image": str(img_b64), 'ip_address': ip_address})
         return SexInfo(**response.json())
         
-    def predict_dfa(self, texts):
-        response = requests.post(self.dfa, json={"texts": texts})
-        print(response.json())
-        return TextInfo(**requests.post(self.dfa, json={"text": texts}).json())
+    def predict_dfa(self, texts:list, ip_address:str):
+        return TextInfo(**requests.post(self.dfa, json={"text": texts,"ip_adderss": ip_address}).json())
     
-    def predict_all(self, path):
-        reponse = requests.post(self.all, json={"image": read_b64(path), 'ip_address': "cctv-005-01"})
+    def predict_all(self, path:str, ip_address:str):
+        reponse = requests.post(self.all, json={"image": read_b64(path), 'ip_address': ip_address})
         return reponse.json()
+    
+    def predict_video(self, path:str):
+        with open(path, 'rb') as f:
+            file_data = f.read()
+            response = requests.post(self.video, files={'file': file_data})
+        return response.json()
 
 
 if __name__ == "__main__":
     fps = [ "/Users/wanghuan/Projects/anomaly/client/tests/sex.png", "/Users/wanghuan/Projects/anomaly/client/tests/test.png",]#"./tests/sex.png",
     
+    m = BackendClient()
+    
     for fp in fps:
 
-        print("Image: ", fp)
-        m = BackendClient()
-        res = m.predict_ocr(fp)
-        print("OCR Result:")
-        print(res)
-        print("\n")
+        # print("Image: ", fp)
+        
+        # res = m.predict_ocr(fp)
+        # print("OCR Result:")
+        # print(res)
+        # print("\n")
 
-        print("Window Result:")
-        res = m.predict_window(fp)
-        print(res)
-        print("\n")
+        # print("Window Result:")
+        # res = m.predict_window(fp)
+        # print(res)
+        # print("\n")
 
-        print("Sex Result:")
-        res = m.predict_sex(fp)
-        print(res)
-        print("\n")
+        # print("Sex Result:")
+        # res = m.predict_sex(fp)
+        # print(res)
+        # print("\n")
 
         
-        all_result = m.predict_all(fp)
+        all_result = m.predict_all(fp, ip_address = "fuck, what is that")
         print(all_result)
+        
+        
+        text_result = m.predict_dfa(texts = ['你妈逼，你是傻逼吧，卧槽，草泥马'], ip_address = 'www.pornlove.net')
+        print(text_result)
+    
+    
+
+    m.predict_video("/Users/wanghuan/Desktop/ anomaly material/file1.mp4")
